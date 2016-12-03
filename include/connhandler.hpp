4c7f7f2b4c3116af3ef76e3f4f5935310eede27a
Rasmus Karlsson <pajlada@bithack.se>
2016-12-03 01:04:43 +0000

    Properly formatted files

--- include/connhandler.hpp
+++ include/connhandler.hpp
@@ -21,15 +21,16 @@ public:
     ConnHandler(const std::string &pass, const std::string &nick);
     ~ConnHandler();
 
-    void joinChannel(const std::string&);
-    void leaveChannel(const std::string&);
+    void joinChannel(const std::string &);
+    void leaveChannel(const std::string &);
     void run();
 
     // user should be a const string reference
-    void handleCommands(std::string& user, const std::string& channel, std::string& msg);
+    void handleCommands(std::string &user, const std::string &channel,
+                        std::string &msg);
 
     // Send message to channel
-    void sendMsg(const std::string& channel, const std::string& message);
+    void sendMsg(const std::string &channel, const std::string &message);
 
     // It's not a map of channel sockets, it's a map of channels.
     // I would just rename this to channels
@@ -39,16 +40,17 @@ public:
 
     // Iterator for twitch chat server endpoints
     asio::ip::tcp::resolver::iterator twitch_it;
-    
+
     // Login details
     std::string pass;
     std::string nick;
+
 private:
     // What does this mutex do?
     // Naming it "mtx" tells me nothing, other than that it's a mutex
     // But I already know that because of its type
     std::mutex mtx;
-    
+
     // A bool for quit? checking
     std::atomic<bool> quit;
 
@@ -56,13 +58,11 @@ private:
     // TODO: rename to ioService
     asio::io_service io_s;
 
-
     // Dummy work that we can start/stop at will to control the ioService
     std::unique_ptr<asio::io_service::work> dummywork;
-    
-    //Thread which decreases the messageCount on all Channels
+
+    // Thread which decreases the messageCount on all Channels
     std::thread msgDecreaser;
 };
-
 
 #endif
